6126eac62840a3aa2ef7392efb9e079ed155d397
Alfie Brown <alfiecbrown@gmail.com>
2025-06-25 13:21:02 +0100

    fix: fix issue with recaptcha

--- src/components/brochure-popup.ts
+++ src/components/brochure-popup.ts
@@ -4,18 +4,30 @@ const cookieHelper = new CookieHelper();
 
 export default class BrochurePopup {
   private readonly emailSignupDialog: HTMLDialogElement | null;
+  private readonly emailSignupBackdrop: HTMLDivElement | null;
+  private readonly emailSignupCloseForm: HTMLFormElement | null | undefined;
 
   constructor() {
     this.emailSignupDialog = document.querySelector("#brochure-download");
+    this.emailSignupBackdrop = document.querySelector(".email-signup_backdrop");
+    this.emailSignupCloseForm = this.emailSignupDialog?.querySelector(
+      "form[method='dialog']"
+    );
   }
 
   setup() {
-    if(this.emailSignupDialog == null) return;
+    if (this.emailSignupDialog == null) return;
 
     const cookieSet = this.checkForCookie();
-    if(cookieSet) return;
+    if (cookieSet) return;
     this.showDialog();
     this.setEmailSignupCookie();
+
+    if (this.emailSignupCloseForm) {
+      this.emailSignupCloseForm.addEventListener("submit", (e) => {
+        this.handleDialogClose();
+      });
+    }
   }
 
   checkForCookie() {
@@ -25,15 +37,23 @@ export default class BrochurePopup {
 
   showDialog() {
     setTimeout(() => {
-      this.emailSignupDialog?.showModal();
-    }, 3000)
+      if (!this.emailSignupBackdrop || !this.emailSignupDialog) return;
+      this.emailSignupDialog.show();
+      this.emailSignupDialog.querySelector("button")?.focus();
+      this.emailSignupBackdrop.style.display = "block";
+    }, 3000);
+  }
+
+  handleDialogClose() {
+    if (!this.emailSignupBackdrop) return;
+    this.emailSignupBackdrop.style.display = "none";
   }
 
   setEmailSignupCookie() {
     cookieHelper.setCookie({
       cookieName: "brochure-shown",
       cookieValue: "true",
-      expirationDays: 30
-    })
+      expirationDays: 30,
+    });
   }
 }
